dfbc26aa31e4136de34e87a4cc651485cf2fd660
ABNmmd <moha.ww.ha@gmail.com>
2024-11-08 22:10:18 +0100

    Reservation context

--- src/contexts/ReservationContext.jsx
+++ src/contexts/ReservationContext.jsx
@@ -6,7 +6,24 @@ const ReservationContext = createContext();
 
 const ReservationProvider = ({ children }) => {
 
-    <ReservationContext.Provider value={{  }}>
+    // create reservation
+    const createReservation = async (data) => {
+        try {
+            const response = await api.post('/reservation', data);
+            return response.data;
+        } catch (error) {
+            console.error('Error creating Reservation', error);
+        }
+    }
+
+    <ReservationContext.Provider
+        value={{
+            createReservation,
+            getReservation,
+            updateReservation,
+            getReservationHistory,
+            createReservationHistory
+        }}>
         {children}
     </ReservationContext.Provider>
 }
