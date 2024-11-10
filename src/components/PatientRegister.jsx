d70d5e2b5b6643e34195f88313a61718f1e474fd
ABNmmd <moha.ww.ha@gmail.com>
2024-11-10 15:40:15 +0100

    patient regeter component

--- src/components/PatientRegister.jsx
+++ src/components/PatientRegister.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
 function PatientRegister() {
-  return (
-    <div>PatientRegister</div>
-  )
+    return (
+        <div>PatientRegister</div>
+    )
 }
 
 export default PatientRegister
