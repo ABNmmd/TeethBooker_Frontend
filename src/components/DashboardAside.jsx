61fb7d2a094014df2edf3cc346780e38d227b201
ABNmmd <moha.ww.ha@gmail.com>
2024-11-14 23:24:47 +0100

    patient dashboard component

---
+++ src/components/DashboardAside.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function DashboardAside() {
+    return (
+        <aside>
+            <h1>TeethBooker</h1>
+            <ul>
+                
+            </ul>
+        </aside>
+    )
+}
+
+export default DashboardAside;
--- src/components/PatientDashboard.jsx
+++ src/components/PatientDashboard.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import DashboardAside from './DashboardAside';
 
 function PatientDashboard() {
     return (
-        <div>PatientDashboard</div>
+        <div className="flex">
+            <DashboardAside />
+            <div className="flex-1 p-4">
+                <h1 className="text-3xl font-bold mb-4">Patient Dashboard</h1>
+            </div>
+        </div>
     )
 }
 
