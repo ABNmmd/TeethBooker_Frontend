0ff9ef78e18c9f32d10fc57cb49a4956124772af
ABNmmd <moha.ww.ha@gmail.com>
2024-11-16 00:33:58 +0100

    patient dashboard page

--- src/components/DashboardAside.jsx
+++ src/components/DashboardAside.jsx
@@ -1,25 +1,53 @@
 import React from 'react'
+import { RxDashboard } from "react-icons/rx";
+import { MdOutlineExpandMore, MdArrowForwardIos } from "react-icons/md";
 
-function DashboardAside({ arr }) {
+function DashboardAside({ arr = [] }) {
     return (
-        <aside>
-            <h1>TeethBooker</h1>
-            <ul>
-                <li>
-                    <a href="/dashboard">Dashboard</a>
-                </li>
-                {arr &&
-                    arr.map((item, index) => (
-                        <li key={index}>
-                            <a href={item}>
-                                
-                                {item}
-                            </a>
-                        </li>
-                    )
-                    )
-                }
-            </ul>
+        <aside className="bg-white h-screen w-fit p-4 shadow-md flex flex-col">
+            <h1 className="text-2xl font-bold mb-10 text-center">TeethBooker</h1>
+            <nav className='flex flex-col flex-grow'>
+                <ul className="flex-grow space-y-4 mb-auto px-3">
+                    <li className="">
+                        <a href="/dashboard" aria-label="Dashboard"
+                            className="flex items-center text-gray-800 hover:text-blue-600 transition-colors"
+                        >
+                            <RxDashboard className="mr-4 text-gray-600" />
+                            Dashboard
+                        </a>
+                    </li>
+                    {
+                        arr.map((item, index) => (
+                            <li key={index} className="">
+                                <button
+                                    onClick={() => console.log(`Clicked on ${item.name}`)}
+                                    className="flex items-center justify-between p-2 w-full text-gray-800 hover:text-blue-600 rounded-lg transition-colors focus:outline-none"
+                                >
+                                    <div className="flex items-center">
+                                        {item.icon}
+                                        <span className="ml-2">{item.name}</span>
+                                    </div>
+                                    <MdArrowForwardIos className="text-gray-600" />
+                                </button>
+                            </li>
+                        )
+                        )
+                    }
+                </ul>
+                <div className="flex-grow-0 flex items-center mt-4">
+                    <img src="https://i.pravatar.cc/100" alt="User Avatar" className="w-10 h-10 rounded-full object-cover" />
+                    <div className="ml-4">
+                        <p className="text-gray-800 text-sm">John Doe</p>
+                        <p className="text-gray-600 text-sm">john.doe@example.com</p>
+                    </div>
+                    <button
+                        aria-label="Expand user options"
+                        className="ml-4 font-bold"
+                    >
+                        <MdOutlineExpandMore />
+                    </button>
+                </div>
+            </nav>
         </aside>
     )
 }
--- src/components/PatientDashboard.jsx
+++ src/components/PatientDashboard.jsx
@@ -1,17 +1,22 @@
 import React from 'react'
 import DashboardAside from './DashboardAside';
+import { FaUserDoctor } from "react-icons/fa6";
+import { FaHistory } from "react-icons/fa";
 
 const arr = [
-    'Doctors',
-    'History',
+    {name: 'Doctors', icon: <FaUserDoctor className='mr-4 text-gray-600' />},
+    {name: 'History', icon: <FaHistory className='mr-4 text-gray-600' />},
 ];
 
 function PatientDashboard() {
     return (
-        <div className="flex">
+        <div className="flex min-h-screen bg-white">
             <DashboardAside arr={arr} />
-            <div className="flex-1 p-4">
+            <div className="flex-1 p-6 bg-gray-100">
                 <h1 className="text-3xl font-bold mb-4">Patient Dashboard</h1>
+                <p className="text-gray-700">
+                    Welcome to your dashboard! Here you can manage your appointments, view your medical history, and more.
+                </p>
             </div>
         </div>
     )
