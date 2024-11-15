f6a513b9d435299726e3d70aa62a572d42750d9a
ABNmmd <moha.ww.ha@gmail.com>
2024-11-15 22:38:11 +0100

    patient dashboard page

--- src/components/DashboardAside.jsx
+++ src/components/DashboardAside.jsx
@@ -1,11 +1,24 @@
 import React from 'react'
 
-function DashboardAside() {
+function DashboardAside({ arr }) {
     return (
         <aside>
             <h1>TeethBooker</h1>
             <ul>
-                
+                <li>
+                    <a href="/dashboard">Dashboard</a>
+                </li>
+                {arr &&
+                    arr.map((item, index) => (
+                        <li key={index}>
+                            <a href={item}>
+                                
+                                {item}
+                            </a>
+                        </li>
+                    )
+                    )
+                }
             </ul>
         </aside>
     )
--- src/components/PatientDashboard.jsx
+++ src/components/PatientDashboard.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import DashboardAside from './DashboardAside';
 
+const arr = [
+    'Doctors',
+    'History',
+];
+
 function PatientDashboard() {
     return (
         <div className="flex">
-            <DashboardAside />
+            <DashboardAside arr={arr} />
             <div className="flex-1 p-4">
                 <h1 className="text-3xl font-bold mb-4">Patient Dashboard</h1>
             </div>
