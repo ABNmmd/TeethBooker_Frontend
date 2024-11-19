2976ef7d383b318f895815781b4b87736850832f
ABNmmd <moha.ww.ha@gmail.com>
2024-11-19 22:49:20 +0100

    patient dashboard

---
+++ src/components/DashboardDefult.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function DashboardDefult() {
+    return (
+        <div>DashboardDefult</div>
+    )
+}
+
+export default DashboardDefult;
--- src/components/PatientDashboard.jsx
+++ src/components/PatientDashboard.jsx
@@ -1,11 +1,14 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
 import DashboardAside from './DashboardAside';
 import { FaUserDoctor } from "react-icons/fa6";
 import { FaHistory, FaCalendarAlt } from "react-icons/fa";
 import { IoSettingsOutline } from "react-icons/io5";
 
-import MyAppointments from './MyAppointments'
+import MyAppointments from './MyAppointments';
 import DoctorsListing from './DoctorsListing';
+import PatientHistory from './PatientHistory';
+import Settings from './Settings';
+import DashboardDefult from './DashboardDefult';
 
 const arr = [
     { id: 1, name: 'My Appointments', icon: <FaCalendarAlt className='mr-4' />, component: <MyAppointments /> },
@@ -22,7 +25,7 @@ function PatientDashboard() {
             <div className="flex-1 p-6 bg-gray-100">
                 {
                     page == 0
-                        ? <MainDashboardPatient />
+                        ? <DashboardDefult />
                         : arr[page - 1].component
                 }
             </div>
